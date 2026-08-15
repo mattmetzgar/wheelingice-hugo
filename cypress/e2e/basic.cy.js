78fd898a60f1aea867c0cfc394fd516055c3de17
mattmetzgar <55600169+mattmetzgar@users.noreply.github.com>
2026-08-15 17:56:24 -0400

    Switch site to Hugo Bear Blog theme

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,34 +1,22 @@
-describe("empty spec", () => {
+describe("Wheeling Ice", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("opens the index page", () => {
-    cy.get("h1").contains("Great coffee with a conscience");
-  });
-
-  it("navigates to the product page", () => {
-    cy.get('a[href="/products"]').eq(0).click();
-    cy.url().should("include", "/products");
-    cy.get("h1").contains(/Our Coffee/i);
-  });
-
-  it("navigates to the values page", () => {
-    cy.get('a[href="/values"]').eq(0).click();
-    cy.url().should("include", "/values");
-    cy.get("h1").contains(/Values/i);
+    cy.get("h1").contains("Music for the People");
   });
 
   it("navigates to the blog page", () => {
-    cy.get('a[href="/post"]').eq(0).click();
-    cy.url().should("include", "/post");
-    cy.get("h1").contains(/Latest Stories/i);
+    cy.get('a[href="/blog/"]').eq(0).click();
+    cy.url().should("include", "/blog/");
+    cy.get("ul.blog-posts").should("be.visible");
   });
 });
 
 describe("validate blog", () => {
   it("should have only 3 blog posts by default", () => {
-    cy.visit("/post");
-    cy.get("ul#blog-list li").should("have.length", 3);
+    cy.visit("/blog/");
+    cy.get("ul.blog-posts li").should("have.length", 3);
   });
 });
